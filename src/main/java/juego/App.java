352015910e6864c1a0cbe69bb64ee93920d52ddb
MaLiKoDi <maximiliano.n.nieto@gmail.com>
2023-10-09 21:50:33 -0300

    tercer commit?

---
+++ src/main/java/juego/App.java
@@ -0,0 +1,12 @@
+package juego;
+
+public class App {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hola");
+		System.out.println("Hola");
+		System.out.println("Hola");
+	}
+
+}
